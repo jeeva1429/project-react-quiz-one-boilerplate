a8365ec689733d3712e857256c9887861f85c127
jeeva1429 <116168359+jeeva1429@users.noreply.github.com>
2023-03-22 09:34:20 +0530

    Update App.js

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
      <Routes>
-        <Route path='/home' element={<Home/>}></Route>
+        <Route path='/' element={<Home/>}></Route>
         <Route path='/quiz' element={<Quiz />}></Route>
         <Route path='/result' element={<Result />}></Route>
     </Routes>
